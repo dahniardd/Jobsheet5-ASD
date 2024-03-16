25c8325048ac221e591b04a83822a593828f76c9
dahniardd <dahniardvn09@gmail.com>
2024-03-16 21:59:41 +0700

    MainPangkat06 Modifikasi

--- MainPangkat06.java
+++ MainPangkat06.java
@@ -11,7 +11,7 @@ public class MainPangkat06 {
     Pangkat06 [] png = new Pangkat06[elemen];
 
     for (int i = 0; i < elemen; i++) {
-        png[i] = new Pangkat06();
+        png[i] = new Pangkat06(i, i);
         System.out.print("Masukkan nilai yang akan dipangkatkan ke-"+(i+1)+" : ");
         png[i].nilai = sc.nextInt();
         System.out.print("Masukkan nilai pemangkat ke-"+(i+1)+" : ");
@@ -20,12 +20,12 @@ public class MainPangkat06 {
     System.out.println("================================================");
     System.out.println("Hasil Pangkat dengan Bruce Force");
     for (int i = 0; i < elemen; i++) {
-        System.out.println("Nilai "+png[i].nilai+" pangkat "+png[i].pangkat+" adalah : "+png[i].pangkatBF(png[i].nilai,png[i].pangkat));
+        System.out.println("Nilai "+png[i].nilai+" pangkat "+png[i].pangkat+" adalah : "+png[i].pangkatBF());
     }
     System.out.println("================================================");
     System.out.println("Hasil Pangkat dengan Divide and Conquer");
     for (int i = 0; i < elemen; i++) {
-        System.out.println("Nilai "+png[i].nilai+" pangkat "+png[i].pangkat+" adalah : "+png[i].pangkatDC(png[i].nilai,png[i].pangkat));
+        System.out.println("Nilai "+png[i].nilai+" pangkat "+png[i].pangkat+" adalah : "+png[i].pangkatDC());
     }
     System.out.println("================================================");
 }
