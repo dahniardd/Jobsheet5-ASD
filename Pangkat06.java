447fd9023fa397fbdbf2855c530a1fd51c280f1e
dahniardd <dahniardvn09@gmail.com>
2024-03-16 21:59:17 +0700

    Pangkat06 modifikasi

--- Pangkat06.java
+++ Pangkat06.java
@@ -3,25 +3,37 @@ package Minggu5;
 public class Pangkat06 {
     public int nilai, pangkat;
 
-    public int pangkatBF(int a, int n) {
-        int hasil=1;
-        for (int i = 0; i < n; i++) {
-            hasil = hasil * a;
+        public Pangkat06(int nilai, int pangkat) {
+            this.nilai = nilai;
+            this.pangkat = pangkat;
         }
-        return hasil;
+        public int pangkatBF() {
+            int hasil = 1;
+            for (int i = 0; i < pangkat; i++) {
+                hasil *= nilai;
+            }
+            return hasil;
+        }
+        public int pangkatDC() {
+            if (pangkat == 0) {
+                return 1;
+            } else if (pangkat % 2 == 1) {
+                return nilai * pangkatDC() * pangkatDC();
+            } else {
+                return pangkatDC() * pangkatDC();
+            }
+        }
+            public static void main(String[] args) {
+            int a = 2;
+            int n = 5;
+    
+            Pangkat06 pangkatObjek = new Pangkat06(a, n);
+    
+            int hasilBF = pangkatObjek.pangkatBF();
+            int hasilDC = pangkatObjek.pangkatDC();
+    
+            System.out.println("Pangkat " + a + " ^ " + n + " (BF): " + hasilBF);
+            System.out.println("Pangkat " + a + " ^ " + n + " (DC): " + hasilDC);
+        }
     }
-    public int pangkatDC(int a, int n) {
-        if (n==0) {
-            return 1;
-        }
-        else
-        {
-            if(n%2==1)
-                return (pangkatDC(a,n/2) *pangkatDC(a,n/2) *a);
-            else
-                return (pangkatDC(a,n/2) *pangkatDC(a,n/2));
-        }
-    }
-
-}
 
